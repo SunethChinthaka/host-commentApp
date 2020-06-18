7f2038b3af6393581654ce9db1f36fcf22a7dd05
SunethChinthaka <sunethedu@gmail.com>
2020-06-18 21:42:37 +0530

    online db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //db connection
-mongoose.connect('mongodb://localhost:27017/onlineShoppingCart',{useNewUrlParser:true,useUnifiedTopology:true}, err => {
+mongoose.connect('mongodb+srv://admin:admin123@comment-deddt.mongodb.net/test',{useNewUrlParser:true,useUnifiedTopology:true}, err => {
     if (!err)
         console.log('MongoDB Connection Succeeded')
     else
