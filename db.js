3b052c10458de289d83a2540e8efab6e1e170ebb
SunethChinthaka <sunethedu@gmail.com>
2020-06-18 20:27:43 +0530

    db changed

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 //db connection
-mongoose.connect('mongodb://localhost:27017/onlineShoppingCart',{useNewUrlParser:true,useUnifiedTopology:true}, err => {
+mongoose.connect('mongodb+srv://admin:admin123@comment-deddt.mongodb.net/test',{useNewUrlParser:true,useUnifiedTopology:true}, err => {
     if (!err)
         console.log('MongoDB Connection Succeeded')
     else
         console.log('MongoDB Connection Error:' + JSON.stringify(err.undefined, 2))
 
 })
+
+
+//////Local DB/////
+
+// mongodb://localhost:27017/onlineShoppingCart
