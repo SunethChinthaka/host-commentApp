3e30150614daa191f86cb49a2ea8ff29408b0b3f
SunethChinthaka <sunethedu@gmail.com>
2020-06-18 20:47:46 +0530

    port changed

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const path = require('path')
 
 app.use(bodyParser.json())
 app.use(cors({origin:'http://localhost:3000'}))
-app.listen(4000,()=>console.log('Server started at : 4000'))
+// app.listen(5000,()=>console.log('Server started at : 5000'))
+
+app.listen(process.env.PORT || 5000, function() {
+    console.log("Server started.......");
+});
 
 if(process.env.NODE_ENV=='production'){
     app.use(express.static('client/build'));
